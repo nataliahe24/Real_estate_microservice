918d84be6ccc112d33967f06d2128cacc1cd0e55
Natalia Henao <nataliahenaor24@gmail.com>
2025-03-13 00:07:56 -0500

    refactor: add constants to use in LocationModel

--- src/main/java/com/powerup/realestate/location/domain/model/LocationModel.java
+++ src/main/java/com/powerup/realestate/location/domain/model/LocationModel.java
@@ -1,12 +1,14 @@
 package com.powerup.realestate.location.domain.model;
 
 import com.powerup.realestate.category.domain.exceptions.DescriptionMaxSizeExceededException;
-import com.powerup.realestate.category.domain.utils.constants.DomainConstants;
 import com.powerup.realestate.location.domain.exceptions.CityMaxSizeExceededException;
 import com.powerup.realestate.location.domain.exceptions.DepartmentMaxSizeExceededException;
+import com.powerup.realestate.location.domain.utils.constants.LocationDomainConstants;
 import lombok.Getter;
 
 import java.util.Objects;
+
+import static com.powerup.realestate.location.domain.utils.constants.LocationDomainConstants.*;
 
 public class LocationModel {
 
@@ -16,12 +18,17 @@ public class LocationModel {
     private @Getter String description;
 
     public LocationModel(Long id, String city, String department, String description) {
-        if (city.length() > 50) {
+        if (city.length() > CityMaxSize) {
             throw new CityMaxSizeExceededException();
         }
-        if (department.length() > 120) {
+        if (department.length() > DepartmentMaxSize) {
             throw new DepartmentMaxSizeExceededException();
         }
+        if (description.length() > DescriptionMaxSize ) {
+            throw new DescriptionMaxSizeExceededException();
+       }
+
+
         this.id = id;
         this.city = city;
         this.department = department;
@@ -29,17 +36,14 @@ public class LocationModel {
     }
 
     public void setDescription(String description) {
-        if (description.length() > 90) throw new DescriptionMaxSizeExceededException();
-        this.description = Objects.requireNonNull(description,  DomainConstants.FIELD_DESCRIPTION_NULL_MESSAGE);
+        this.description = Objects.requireNonNull(description,  LocationDomainConstants.FIELD_DESCRIPTION_NULL_MESSAGE);
    }
     public void setDepartment(String department) {
-        if (department.length() > 120) throw new DepartmentMaxSizeExceededException();
-        this.department = Objects.requireNonNull(department,  DomainConstants.FIELD_DESCRIPTION_NULL_MESSAGE);
+        this.department = Objects.requireNonNull(department,  LocationDomainConstants.FIELD_DEPARTMENT_NULL_MESSAGE);
     }
 
     public void setCity(String city) {
-        if (city.length() > 50) throw new CityMaxSizeExceededException();
-        this.city = Objects.requireNonNull(city,  DomainConstants.FIELD_DESCRIPTION_NULL_MESSAGE);
-    }
+        this.city = Objects.requireNonNull(city,  LocationDomainConstants.FIELD_CITY_NULL_MESSAGE);
+        }
 
-}
+    }
--- src/main/java/com/powerup/realestate/location/infrastructure/endpoints/rest/LocationController.java
+++ src/main/java/com/powerup/realestate/location/infrastructure/endpoints/rest/LocationController.java
@@ -14,10 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/Location")
+@RequestMapping("/api/v1/location")
 @RequiredArgsConstructor
 public class LocationController {
 
