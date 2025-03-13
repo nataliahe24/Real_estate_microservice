aee64c49856c7cd20a60477aa0f343687d120c92
Natalia Henao <nataliahenaor24@gmail.com>
2025-03-13 17:09:06 -0500

    refactor: renaming DEPARTMENT_EXISTS_EXCEPTION in ExceptionConstants

--- src/main/java/com/powerup/realestate/location/infrastructure/exceptionshandler/ExceptionConstants.java
+++ src/main/java/com/powerup/realestate/location/infrastructure/exceptionshandler/ExceptionConstants.java
@@ -4,6 +4,6 @@ public final class ExceptionConstants {
     private ExceptionConstants(){}
 
     public static final String NAME_MAX_SIZE_MESSAGE = "The name of the Location can not exceed 50 characters";
-    public static final String DESCRIPTION_MAX_SIZE_MESSAGE = "The description of the Location can not exceed 90 characters";
+    public static final String DESCRIPTION_MAX_SIZE_MESSAGE = "The description of the Location can not exceed 50 characters";
     public static final String DEPARTMENT_EXISTS_EXCEPTION = "The department already exists";
 }
