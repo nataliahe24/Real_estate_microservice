813ba4b8ded316a61315e4ff3eeeae83772ded63
Natalia Henao <nataliahenaor24@gmail.com>
2025-03-19 00:41:54 -0500

    feat: get getLocations() methods

--- src/main/java/com/powerup/realestate/location/application/services/impl/LocationServiceImpl.java
+++ src/main/java/com/powerup/realestate/location/application/services/impl/LocationServiceImpl.java
@@ -1,11 +1,13 @@
 package com.powerup.realestate.location.application.services.impl;
 
+import com.powerup.realestate.location.application.dto.response.LocationResponse;
 import com.powerup.realestate.location.application.dto.request.SaveLocationRequest;
 import com.powerup.realestate.location.application.dto.response.SaveLocationResponse;
 import com.powerup.realestate.location.application.mappers.LocationDtoMapper;
 import com.powerup.realestate.location.domain.ports.in.LocationServicePort;
 import com.powerup.realestate.location.application.services.LocationService;
 import com.powerup.realestate.commons.configurations.utils.Constants;
+import com.powerup.realestate.location.domain.utils.constants.page.PageResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +23,10 @@ public class LocationServiceImpl implements LocationService {
     public SaveLocationResponse save(SaveLocationRequest request) {
         locationServicePort.save(locationDtoMapper.requestToModel(request));
         return new SaveLocationResponse(Constants.SAVE_LOCATION_RESPONSE_MESSAGE, LocalDateTime.now());
+    }
+
+    @Override
+    public PageResult<LocationResponse> getLocations(String searchText,Integer page, Integer size, boolean orderAsc) {
+        return locationDtoMapper.modelListToResponseList(locationServicePort.getLocations(searchText, page, size, orderAsc));
     }
 }
