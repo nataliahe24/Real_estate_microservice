8f89f9944205a474238e4698a378b98978b44829
Natalia Henao <nataliahenaor24@gmail.com>
2025-03-20 02:09:43 -0500

    fear: add LocationUseCaseTest

--- src/test/java/com/powerup/realestate/location/domain/usecases/LocationUseCaseTest.java
+++ src/test/java/com/powerup/realestate/location/domain/usecases/LocationUseCaseTest.java
@@ -3,11 +3,14 @@ package com.powerup.realestate.location.domain.usecases;
 import com.powerup.realestate.location.domain.exceptions.DepartmentAlreadyExistsException;
 import com.powerup.realestate.location.domain.model.LocationModel;
 import com.powerup.realestate.location.domain.ports.out.LocationPersistencePort;
+import com.powerup.realestate.location.domain.utils.constants.page.PageResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -58,5 +61,38 @@ class LocationUseCaseTest {
         assertThrows(DepartmentAlreadyExistsException.class, () -> locationUseCase.save(locationModel));
 
         verify(locationPersistencePort, never()).save(locationModel);
+    }
+
+    @Test
+    void getLocations_ShouldReturnSortedList_WhenOrderIsAsc() {
+        LocationModel locationModel1 = new LocationModel(
+                1L,
+                "City1",
+                "Description City1",
+                "Department1",
+                "Description Department1"
+        );
+        LocationModel locationModel2 = new LocationModel(
+                2L,
+                "City2",
+                "Description City2",
+                "Department2",
+                "Description Department2"
+        );
+
+        String searchText = "city or department";
+        Integer page = 1;
+        Integer size = 2;
+        int totalElements = 3;
+        boolean orderAsc = true;
+        List<LocationModel> locationPaginationMock = List.of(locationModel1, locationModel2);
+        PageResult<LocationModel> pageResultMock = new PageResult<>(locationPaginationMock, page, size, totalElements);
+
+        when(locationPersistencePort.getLocations(searchText, page, size, orderAsc)).thenReturn(pageResultMock);
+
+        PageResult<LocationModel> paginatedLocationList = locationUseCase.getLocations(searchText, page, size, orderAsc);
+
+        verify(locationPersistencePort).getLocations(searchText, page, size, orderAsc);
+        assertEquals(pageResultMock, paginatedLocationList);
     }
 }
