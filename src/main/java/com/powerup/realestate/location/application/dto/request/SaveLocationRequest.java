85ca2a07ee14b08f892130db1b958c5cf5b8b671
Natalia Henao <nataliahenaor24@gmail.com>
2025-03-20 12:58:42 -0500

    refactor: id from SaveLocationRequest

--- src/main/java/com/powerup/realestate/location/application/dto/request/SaveLocationRequest.java
+++ src/main/java/com/powerup/realestate/location/application/dto/request/SaveLocationRequest.java
@@ -1,4 +1,4 @@
 package com.powerup.realestate.location.application.dto.request;
 
-public record SaveLocationRequest(Long id, String city, String descriptionCity, String department, String descriptionDepartment) {
+public record SaveLocationRequest(String city, String descriptionCity, String department, String descriptionDepartment) {
 }
