17b84260091f2d685dfa3394e52752ca350fd74f
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-01 16:40:40 -0500

    feat: add PropertyDomainContants

---
+++ src/main/java/com/powerup/realestate/properties/domain/utils/constants/PropertyDomainContants.java
@@ -0,0 +1,13 @@
+package com.powerup.realestate.properties.domain.utils.constants;
+
+
+public final class PropertyDomainContants {
+    public static final String FIELD_NAME_NULL_MESSAGE = "El campo 'name' no puede estar vacío.";
+    public static final String FIELD_DESCRIPTION_NULL_MESSAGE = "El campo 'description' no puede estar vacío.";
+    public static final String FIELD_CATEGORY_NULL_MESSAGE = "El campo 'description' no puede estar vacío.";
+    public static final String FIELD_PRICE_NULL_MESSAGE = "El campo 'description' no puede estar vacío.";
+    public static final String FIELD_LOCATION_NULL_MESSAGE = "El campo 'description' no puede estar vacío.";
+    public static final String FIELD_ACTIVE_PUBLICATION_DATE_NULL_MESSAGE = "El campo 'description' no puede estar vacío.";
+    public static final String FIELD_PUBLICATION_STATUS_NULL_MESSAGE = "El campo 'description' no puede estar vacío.";
+    public static final String FIELD_PUBLICATION_DATE_NULL_MESSAGE = "El campo 'description' no puede estar vacío.";
+}
