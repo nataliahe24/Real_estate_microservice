a9d24eb065504cb0aca85e1af2a8e82a2388e376
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-01 16:39:06 -0500

    feat: add Exceptions

---
+++ src/main/java/com/powerup/realestate/properties/domain/exceptions/InvalidActivePublicationDateException.java
@@ -0,0 +1,7 @@
+package com.powerup.realestate.properties.domain.exceptions;
+
+public class InvalidActivePublicationDateException extends RuntimeException {
+    public InvalidActivePublicationDateException() {
+        super();
+    }
+}
