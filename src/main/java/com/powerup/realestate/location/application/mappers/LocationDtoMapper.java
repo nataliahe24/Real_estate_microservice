e8c611e9c123b01139c504d7e7fbdcc74878cc65
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-04 16:52:32 -0500

    refactor: add mapping to cityId

--- src/main/java/com/powerup/realestate/location/application/mappers/LocationDtoMapper.java
+++ src/main/java/com/powerup/realestate/location/application/mappers/LocationDtoMapper.java
@@ -4,7 +4,10 @@ import com.powerup.realestate.location.application.dto.request.SaveLocationReque
 import com.powerup.realestate.location.application.dto.response.LocationResponse;
 import com.powerup.realestate.location.domain.model.LocationModel;
 import com.powerup.realestate.location.domain.utils.constants.page.PageResult;
+import com.powerup.realestate.location.infrastructure.entities.CityEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.data.domain.Page;
 
@@ -12,8 +15,24 @@ import org.springframework.data.domain.Page;
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface LocationDtoMapper {
+    @Mapping(source = "cityId", target = "cityId", qualifiedByName = "mapCityIdToEntity")
     LocationModel requestToModel(SaveLocationRequest saveLocationRequest);
+    @Mapping(source = "cityId", target = "cityId", qualifiedByName = "mapCityEntityToId")
     LocationResponse modelToResponse(LocationModel locationModel);
     PageResult<LocationResponse> modelListToResponseList(PageResult<LocationModel> locations);
+    @Named("mapCityIdToEntity")
+    default CityEntity mapCityIdToEntity(Long cityId) {
+        if (cityId == null) {
+            return null;
+        }
+        CityEntity cityEntity = new CityEntity();
+        cityEntity.setId(cityId);
+        return cityEntity;
+    }
+
+    @Named("mapCityEntityToId")
+    default Long mapCityEntityToId(CityEntity city) {
+        return (city != null) ? city.getId() : null;
+    }
 
 }
