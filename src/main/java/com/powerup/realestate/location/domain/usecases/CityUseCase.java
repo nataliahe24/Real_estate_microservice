44547168dcb3e7131127a6dc0b01201da5250cfc
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-04 18:47:17 -0500

    feat: CityUseCase

---
+++ src/main/java/com/powerup/realestate/location/domain/usecases/CityUseCase.java
@@ -0,0 +1,31 @@
+package com.powerup.realestate.location.domain.usecases;
+
+import com.powerup.realestate.location.domain.exceptions.CityNonExistentException;
+import com.powerup.realestate.location.domain.ports.in.CityServicePort;
+import com.powerup.realestate.location.domain.ports.out.CityPersistencePort;
+import com.powerup.realestate.location.infrastructure.entities.CityEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CityUseCase implements CityServicePort {
+    private final CityPersistencePort cityPersistencePort;
+
+    @Autowired
+    public CityUseCase(CityPersistencePort cityPersistencePort) {
+        this.cityPersistencePort = cityPersistencePort;
+    }
+
+    @Override
+    public CityEntity findCityByNameIgnoreCaseAndTrim(String cityName) {
+        if (cityName == null || cityName.isEmpty()) {
+            return null; // O lanzar una excepción específica si es necesario
+        }
+        CityEntity cityEntity = cityPersistencePort.findByNameIgnoreCaseAndTrim(cityName);
+        if (cityEntity == null) {
+            throw new CityNonExistentException();
+        }
+        return cityEntity;
+    }
+
+    // Otros métodos relacionados con la gestión de ciudades podrían ir aquí
+}
+
