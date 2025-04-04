fd95bfce3e6b8693b2a4af0edc0eefcb4b358a0f
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-04 18:44:33 -0500

    feat: add CityServicePort in LocationUseCase

--- src/main/java/com/powerup/realestate/location/domain/usecases/LocationUseCase.java
+++ src/main/java/com/powerup/realestate/location/domain/usecases/LocationUseCase.java
@@ -1,24 +1,47 @@
 package com.powerup.realestate.location.domain.usecases;
 
 
+import com.powerup.realestate.location.domain.exceptions.CityNonExistentException;
 import com.powerup.realestate.location.domain.model.LocationModel;
+import com.powerup.realestate.location.domain.ports.in.CityServicePort;
 import com.powerup.realestate.location.domain.ports.in.LocationServicePort;
 import com.powerup.realestate.location.domain.ports.out.LocationPersistencePort;
 import com.powerup.realestate.location.domain.utils.constants.page.PageResult;
+import com.powerup.realestate.location.infrastructure.entities.CityEntity;
 
 
 public class LocationUseCase implements LocationServicePort {
     private final LocationPersistencePort locationPersistencePort;
+    private final CityServicePort cityServicePort;
 
-    public LocationUseCase(LocationPersistencePort locationPersistencePort) {
+    public LocationUseCase(LocationPersistencePort locationPersistencePort, CityServicePort cityServicePort) {
         this.locationPersistencePort = locationPersistencePort;
+        this.cityServicePort = cityServicePort;
+
     }
 
     @Override
     public void save(LocationModel locationModel) {
+        CityEntity cityInfo = locationModel.getCityName(); // Assuming LocationModel has getCityName()
 
+        if (cityInfo != null && cityInfo.getName() != null && !cityInfo.getName().isEmpty()) {
+            try {
+                CityEntity cityEntity = cityServicePort.findCityByNameIgnoreCaseAndTrim(cityInfo.getName());
 
-        locationPersistencePort.save(locationModel);
+                // Create a reference to the CityEntity to establish the relationship
+                CityEntity cityReference = new CityEntity();
+                cityReference.setId(cityEntity.getId());
+                locationModel.setCityName(cityReference); // Assuming LocationModel has setCity(CityEntity)
+
+                locationPersistencePort.save(locationModel);
+
+            } catch (CityNonExistentException e) {
+                throw e; // Re-throw the exception for the calling layer to handle
+            }
+        } else {
+            throw new IllegalArgumentException("City name cannot be null or empty.");
+        }
+
     }
     @Override
     public PageResult<LocationModel> getLocations(String searchText, Integer page, Integer size, boolean orderAsc) {
