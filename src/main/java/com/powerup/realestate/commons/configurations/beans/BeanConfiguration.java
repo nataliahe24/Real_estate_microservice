cff83ff2711540bb280374f5059524c8b33f2784
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-06 16:57:24 -0500

    refactor: BeanConfiguration

--- src/main/java/com/powerup/realestate/commons/configurations/beans/BeanConfiguration.java
+++ src/main/java/com/powerup/realestate/commons/configurations/beans/BeanConfiguration.java
@@ -6,10 +6,15 @@ import com.powerup.realestate.location.domain.usecases.CityUseCase;
 import com.powerup.realestate.location.infrastructure.adapters.persistence.CityPersistenceAdapter;
 import com.powerup.realestate.location.infrastructure.repositories.mysql.CityRepository;
 import com.powerup.realestate.properties.domain.ports.in.CategoryServicePort;
+import com.powerup.realestate.properties.domain.ports.in.PropertyServicePort;
 import com.powerup.realestate.properties.domain.ports.out.CategoryPersistencePort;
+import com.powerup.realestate.properties.domain.ports.out.PropertyPersistencePort;
 import com.powerup.realestate.properties.domain.usecases.CategoryUseCase;
+import com.powerup.realestate.properties.domain.usecases.PropertyUseCase;
 import com.powerup.realestate.properties.infrastructure.adapters.persistence.CategoryPersistenceAdapter;
+import com.powerup.realestate.properties.infrastructure.adapters.persistence.PropertyPersistenceAdapter;
 import com.powerup.realestate.properties.infrastructure.mappers.CategoryEntityMapper;
+import com.powerup.realestate.properties.infrastructure.mappers.PropertyEntityMapper;
 import com.powerup.realestate.properties.infrastructure.repositories.mysql.CategoryRepository;
 import com.powerup.realestate.location.domain.ports.in.LocationServicePort;
 import com.powerup.realestate.location.domain.ports.out.LocationPersistencePort;
@@ -17,6 +22,7 @@ import com.powerup.realestate.location.domain.usecases.LocationUseCase;
 import com.powerup.realestate.location.infrastructure.adapters.persistence.LocationPersistenceAdapter;
 import com.powerup.realestate.location.infrastructure.mappers.LocationEntityMapper;
 import com.powerup.realestate.location.infrastructure.repositories.mysql.LocationRepository;
+import com.powerup.realestate.properties.infrastructure.repositories.mysql.PropertyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,6 +35,14 @@ public class BeanConfiguration {
     private final LocationRepository locationRepository;
     private final LocationEntityMapper locationEntityMapper;
     private final CityRepository cityRepository;
+    private final PropertyRepository propertyRepository;
+    private final PropertyEntityMapper propertyEntityMapper;
+
+
+    @Bean
+    public CategoryPersistencePort categoryPersistencePort() {
+        return new CategoryPersistenceAdapter(categoryRepository, categoryEntityMapper);
+    }
 
     @Bean
     public CategoryServicePort categoryServicePort() {
@@ -37,9 +51,16 @@ public class BeanConfiguration {
     }
 
     @Bean
-    public CategoryPersistencePort categoryPersistencePort() {
-        return new CategoryPersistenceAdapter(categoryRepository, categoryEntityMapper);
+    public LocationPersistencePort locationPersistencePort() {
+        return new LocationPersistenceAdapter(locationRepository, locationEntityMapper);
     }
+
+    @Bean
+    public LocationServicePort locationServicePort(LocationPersistencePort locationPersistencePort, CityServicePort cityServicePort) {
+
+        return new LocationUseCase(locationPersistencePort(), cityServicePort(cityPersistencePort()));
+    }
+
     @Bean
     public CityPersistencePort cityPersistencePort() {
         return new CityPersistenceAdapter(cityRepository);
@@ -49,14 +70,15 @@ public class BeanConfiguration {
     public CityServicePort cityServicePort(CityPersistencePort cityPersistencePort) {
         return new CityUseCase(cityPersistencePort);
     }
-    @Bean
-    public LocationServicePort locationServicePort(LocationPersistencePort locationPersistencePort, CityServicePort cityServicePort) {
 
-        return new LocationUseCase(locationPersistencePort(), cityServicePort(cityPersistencePort()));
+    @Bean
+    public PropertyPersistencePort propertyPersistencePort() {
+        return new PropertyPersistenceAdapter(propertyRepository, propertyEntityMapper);
     }
 
     @Bean
-    public LocationPersistencePort locationPersistencePort() {
-        return new LocationPersistenceAdapter(locationRepository, locationEntityMapper);
+    public PropertyServicePort propertyServicePort() {
+        return  new PropertyUseCase(propertyPersistencePort());
     }
+
 }
