8be106a980e8a3888ca2565711e9a81e467c9b9f
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-07 00:40:42 -0500

    feat: remove imports

--- src/main/java/com/powerup/realestate/properties/domain/usecases/PropertyUseCase.java
+++ src/main/java/com/powerup/realestate/properties/domain/usecases/PropertyUseCase.java
@@ -1,6 +1,11 @@
 package com.powerup.realestate.properties.domain.usecases;
 
+import com.powerup.realestate.commons.configurations.utils.Constants;
+import com.powerup.realestate.location.domain.ports.in.LocationServicePort;
+import com.powerup.realestate.properties.domain.exceptions.CategoryNotFoundException;
+import com.powerup.realestate.properties.domain.exceptions.LocationNotFoundException;
 import com.powerup.realestate.properties.domain.model.PropertyModel;
+import com.powerup.realestate.properties.domain.ports.in.CategoryServicePort;
 import com.powerup.realestate.properties.domain.ports.in.PropertyServicePort;
 import com.powerup.realestate.properties.domain.ports.out.PropertyPersistencePort;
 
