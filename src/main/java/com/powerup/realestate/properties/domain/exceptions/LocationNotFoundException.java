166ed25344b28605e813da4a7e64e670cbdec0e4
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-08 00:05:34 -0500

    feat: add LocationNotFoundException

--- src/main/java/com/powerup/realestate/properties/domain/exceptions/LocationNotFoundException.java
+++ src/main/java/com/powerup/realestate/properties/domain/exceptions/LocationNotFoundException.java
@@ -1,7 +1,7 @@
 package com.powerup.realestate.properties.domain.exceptions;
 
 public class LocationNotFoundException extends RuntimeException {
-    public LocationNotFoundException(String message) {
-        super(message);
+    public LocationNotFoundException() {
+        super();
     }
 }
