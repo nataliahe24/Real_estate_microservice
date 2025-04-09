306ea45a856f430bf7938bf607b2de55b68af2ae
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-09 15:27:55 -0500

    feat : implement validate method

--- src/main/java/com/powerup/realestate/properties/domain/usecases/PropertyUseCase.java
+++ src/main/java/com/powerup/realestate/properties/domain/usecases/PropertyUseCase.java
@@ -1,54 +1,24 @@
 package com.powerup.realestate.properties.domain.usecases;
 
-import com.powerup.realestate.location.domain.model.LocationModel;
-import com.powerup.realestate.location.domain.ports.in.LocationServicePort;
-import com.powerup.realestate.location.domain.ports.out.LocationPersistencePort;
+import com.powerup.realestate.properties.application.services.PropertyValidationService;
 import com.powerup.realestate.properties.domain.exceptions.CategoryNotFoundException;
 import com.powerup.realestate.properties.domain.exceptions.LocationNotFoundException;
-import com.powerup.realestate.properties.domain.model.CategoryModel;
 import com.powerup.realestate.properties.domain.model.PropertyModel;
-import com.powerup.realestate.properties.domain.ports.in.CategoryServicePort;
 import com.powerup.realestate.properties.domain.ports.in.PropertyServicePort;
-import com.powerup.realestate.properties.domain.ports.out.CategoryPersistencePort;
 import com.powerup.realestate.properties.domain.ports.out.PropertyPersistencePort;
 
 
 public class PropertyUseCase implements PropertyServicePort {
     private final PropertyPersistencePort propertyPersistencePort;
-    private final CategoryPersistencePort categoryPersistencePort;
-    private final LocationPersistencePort locationPersistencePort;
+    private final PropertyValidationService propertyValidationService;
 
-    public PropertyUseCase(PropertyPersistencePort propertyPersistencePort, CategoryServicePort categoryServicePort, CategoryPersistencePort categoryPersistencePort, LocationServicePort locationServicePort, LocationPersistencePort locationPersistencePort) {
+    public PropertyUseCase(PropertyPersistencePort propertyPersistencePort, PropertyValidationService propertyValidationService) {
         this.propertyPersistencePort = propertyPersistencePort;
-        this.categoryPersistencePort = categoryPersistencePort;
-        this.locationPersistencePort = locationPersistencePort;
+        this.propertyValidationService = propertyValidationService;
     }
     @Override
-    public void save(PropertyModel propertyModel) {
-        Long locationId = propertyModel.getLocationId();
-        Long categoryId = propertyModel.getCategoryId();
-
-
-        LocationModel locationModel = null;
-        if (locationId != null) {
-            locationModel = locationPersistencePort.findByLocationId(locationId);
-            if (locationModel == null) {
-                throw new LocationNotFoundException();
-            }
-        } else {
-            throw new IllegalArgumentException();
-        }
-
-        CategoryModel categoryModel = null;
-        if (categoryId != null) {
-            categoryModel = categoryPersistencePort.getCategoryById(categoryId);
-            if (categoryModel == null) {
-                throw new CategoryNotFoundException();
-            }
-        } else {
-            throw new IllegalArgumentException();
-        }
-
+    public void save(PropertyModel propertyModel) throws LocationNotFoundException, CategoryNotFoundException {
+        propertyValidationService.validate(propertyModel);
         propertyPersistencePort.save(propertyModel);
     }
 
