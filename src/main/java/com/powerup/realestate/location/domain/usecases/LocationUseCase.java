fa20a5ceba4be41c737fca3002933b79aecdf0eb
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-09 15:33:18 -0500

    refactor: remove from LocationModel and insert to UseCase

--- src/main/java/com/powerup/realestate/location/domain/usecases/LocationUseCase.java
+++ src/main/java/com/powerup/realestate/location/domain/usecases/LocationUseCase.java
@@ -2,13 +2,15 @@ package com.powerup.realestate.location.domain.usecases;
 
 
 import com.powerup.realestate.location.domain.exceptions.CityNonExistentException;
+import com.powerup.realestate.location.domain.exceptions.NeighborhoodNonNullException;
 import com.powerup.realestate.location.domain.model.LocationModel;
 import com.powerup.realestate.location.domain.ports.in.CityServicePort;
 import com.powerup.realestate.location.domain.ports.in.LocationServicePort;
 import com.powerup.realestate.location.domain.ports.out.LocationPersistencePort;
 import com.powerup.realestate.location.domain.utils.constants.page.PageResult;
 import com.powerup.realestate.location.infrastructure.entities.CityEntity;
-import com.powerup.realestate.properties.domain.exceptions.LocationNotFoundException;
+
+import java.util.Optional;
 
 
 public class LocationUseCase implements LocationServicePort {
@@ -24,6 +26,11 @@ public class LocationUseCase implements LocationServicePort {
     @Override
     public void save(LocationModel locationModel) {
         CityEntity cityInfo = locationModel.getCityName();
+        String neighborhood = locationModel.getNeighborhood();
+
+        if (neighborhood == null || neighborhood.trim().isEmpty()) {
+            throw new NeighborhoodNonNullException();
+        }
 
         if (cityInfo != null && cityInfo.getName() != null && !cityInfo.getName().isEmpty()) {
             CityEntity cityEntity = cityServicePort.findCityByNameIgnoreCaseAndTrim(cityInfo.getName());
@@ -41,15 +48,12 @@ public class LocationUseCase implements LocationServicePort {
     }
 
     @Override
-    public LocationModel findByLocationId(Long locationId) {
+    public Optional<LocationModel> findByLocationId(Long locationId) {
         if (locationId == null) {
-            return null;
+            return Optional.empty();
         }
-        LocationModel locationModel = locationPersistencePort.findByLocationId(locationId);
-        if (locationModel == null) {
-            throw new LocationNotFoundException();
-        }
-        return locationModel;
+
+        return locationPersistencePort.findByLocationId(locationId);
     }
 
     @Override
