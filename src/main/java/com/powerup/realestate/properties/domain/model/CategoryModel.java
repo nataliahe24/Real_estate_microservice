a5693ddfcc2f953fbfa6f4133ce1c0847d5a4b85
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-09 15:38:48 -0500

    refactor: remove validations in CategoryModel

--- src/main/java/com/powerup/realestate/properties/domain/model/CategoryModel.java
+++ src/main/java/com/powerup/realestate/properties/domain/model/CategoryModel.java
@@ -1,15 +1,13 @@
 package com.powerup.realestate.properties.domain.model;
 
-import com.powerup.realestate.properties.domain.exceptions.DescriptionMaxSizeExceededException;
-import com.powerup.realestate.properties.domain.exceptions.NameMaxSizeExceededException;
 import com.powerup.realestate.properties.domain.utils.constants.CategoryDomainConstants;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.Setter;
-
 import java.util.Objects;
 
-import static com.powerup.realestate.properties.domain.utils.constants.CategoryDomainConstants.DESCRIPTION_MAX_CHARACTERS;
-import static com.powerup.realestate.properties.domain.utils.constants.CategoryDomainConstants.NAME_MAX_CHARACTERS;
+
+@Getter
 @Builder
 public class CategoryModel {
     @Setter
@@ -18,32 +16,17 @@ public class CategoryModel {
     private String description;
 
     public CategoryModel(Long id, String name, String description) {
-        if (name.length() > NAME_MAX_CHARACTERS) throw new NameMaxSizeExceededException();
-        if (description.length() > DESCRIPTION_MAX_CHARACTERS) throw new DescriptionMaxSizeExceededException();
+
         this.id = id;
-        this.name = Objects.requireNonNull(name, CategoryDomainConstants.FIELD_NAME_NULL_MESSAGE);
-        this.description = Objects.requireNonNull(description,  CategoryDomainConstants.FIELD_DESCRIPTION_NULL_MESSAGE);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
+        this.name = name;
+        this.description = description;
     }
 
     public void setName(String name) {
-        if (name.length() > NAME_MAX_CHARACTERS) throw new NameMaxSizeExceededException();
         this.name = Objects.requireNonNull(name, CategoryDomainConstants.FIELD_NAME_NULL_MESSAGE);
     }
 
-    public String getDescription() {
-        return description;
-    }
-
     public void setDescription(String description) {
-        if (description.length() > DESCRIPTION_MAX_CHARACTERS) throw new DescriptionMaxSizeExceededException();
         this.description = Objects.requireNonNull(description,  CategoryDomainConstants.FIELD_DESCRIPTION_NULL_MESSAGE);
     }
 }
