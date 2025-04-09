70183959dfffda746e34a26d2f5265e8b7892dc6
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-09 15:31:29 -0500

    refactor: rename to LocationModel  and CategoryModel date type

--- src/main/java/com/powerup/realestate/properties/domain/model/PropertyModel.java
+++ src/main/java/com/powerup/realestate/properties/domain/model/PropertyModel.java
@@ -1,55 +1,60 @@
 package com.powerup.realestate.properties.domain.model;
 
+import com.powerup.realestate.location.domain.model.LocationModel;
 import com.powerup.realestate.properties.domain.exceptions.InvalidActivePublicationDateException;
 import com.powerup.realestate.properties.domain.exceptions.InvalidBathroomsException;
 import com.powerup.realestate.properties.domain.exceptions.InvalidRoomsException;
 import com.powerup.realestate.properties.domain.utils.PublicationStatus;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Objects;
-
 import static com.powerup.realestate.properties.domain.utils.constants.PropertyDomainContants.*;
 
 @Getter
 @Builder
 public class PropertyModel {
 
+    @Setter
     private Long id;
     private String name;
     private String description;
-    private Long categoryId;
+    private CategoryModel category;
     private final int rooms;
     private final int bathrooms;
     private BigDecimal price;
-    private Long locationId;
+    private LocationModel location;
     private LocalDate activePublicationDate;
     private PublicationStatus publicationStatus;
     private LocalDate publicationDate;
 
 
-    public PropertyModel(Long id, String name, String description, Long categoryId,
-                         int rooms, int bathrooms, BigDecimal price, Long locationId,
+    public PropertyModel(Long id, String name, String description, CategoryModel category,
+                         int rooms, int bathrooms, BigDecimal price, LocationModel location,
                          LocalDate activePublicationDate, PublicationStatus publicationStatus, LocalDate publicationDate) {
         this.id = id;
         this.name = name;
         this.description = description;
-        this.categoryId = categoryId;
+        this.category = category;
         this.rooms = rooms;
         this.bathrooms = bathrooms;
         this.price = price;
-        this.locationId = locationId;
+        this.location = location;
         this.activePublicationDate = activePublicationDate;
-        this.publicationStatus = publicationStatus;
-        this.publicationDate = publicationDate;
+        this.publicationStatus = publicationStatus != null ? publicationStatus : PublicationStatus.PUBLISHING_PAUSED;
+        this.publicationDate = LocalDate.now();
 
 
         if (rooms < 0) throw new InvalidRoomsException();
+
         if (bathrooms < 0) throw new InvalidBathroomsException();
-        if (activePublicationDate != null &&  activePublicationDate.isAfter(LocalDate.now().plusMonths(1))) throw new InvalidActivePublicationDateException();
+
+        if (activePublicationDate.isAfter(LocalDate.now().plusMonths(1)))
+            throw new InvalidActivePublicationDateException();
 
     }
 
@@ -61,29 +66,22 @@ public class PropertyModel {
             this.description = Objects.requireNonNull(description, FIELD_DESCRIPTION_NULL_MESSAGE);
         }
 
-        public void setCategoryId(Long categoryId) {
-            this.categoryId = Objects.requireNonNull(categoryId, FIELD_CATEGORY_NULL_MESSAGE);
-        }
-
         public void setPrice(BigDecimal price) {
-            this.price = Objects.requireNonNull(price, FIELD_PRICE_NULL_MESSAGE);
-        }
-
-        public void setLocationId(Long locationId) {
-            this.locationId = Objects.requireNonNull(locationId, FIELD_LOCATION_NULL_MESSAGE);
+        this.price = Objects.requireNonNull(price, FIELD_PRICE_NULL_MESSAGE);
         }
 
         public void setActivePublicationDate(LocalDate activePublicationDate) {
-            if (activePublicationDate.isAfter(LocalDate.now().plusMonths(1))) throw new InvalidActivePublicationDateException();
+
+            if (activePublicationDate.isAfter(LocalDate.now().plusMonths(1)))
+                throw new InvalidActivePublicationDateException();
+
             this.activePublicationDate = Objects.requireNonNull(activePublicationDate, FIELD_ACTIVE_PUBLICATION_DATE_NULL_MESSAGE);
         }
 
-        public void setPublicationStatus(PublicationStatus publicationStatus) {
-            this.publicationStatus = Objects.requireNonNull(publicationStatus, FIELD_PUBLICATION_STATUS_NULL_MESSAGE);
-        }
-
-        public void setPublicationDate(LocalDate publicationDate) {
+    public void setPublicationDate(LocalDate publicationDate) {
             this.publicationDate = Objects.requireNonNull(publicationDate, FIELD_PUBLICATION_DATE_NULL_MESSAGE);
         }
 
+    public void setPublicationStatus(PublicationStatus publicationStatus) {
+    }
 }
