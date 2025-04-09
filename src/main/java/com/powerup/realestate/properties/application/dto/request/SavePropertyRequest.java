145a99faff1891e0a1ba544ccc011bdfbeb3f5f1
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-09 15:26:43 -0500

    refactor: remove publicationDate and publicationStatus

--- src/main/java/com/powerup/realestate/properties/application/dto/request/SavePropertyRequest.java
+++ src/main/java/com/powerup/realestate/properties/application/dto/request/SavePropertyRequest.java
@@ -1,10 +1,12 @@
 package com.powerup.realestate.properties.application.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record SavePropertyRequest(String name,String description,Long categoryId,Integer rooms, Integer bathrooms,
-                                 BigDecimal price, Long locationId, LocalDate activePublicationDate,
-                                 String publicationStatus, LocalDate publicationDate) {
+public record SavePropertyRequest(@NotBlank String name,@NotBlank String description,@NotBlank Long category,@NotBlank Integer rooms,@NotBlank Integer bathrooms,
+                                  @NotBlank BigDecimal price, @NotBlank Long location,@NotBlank LocalDate activePublicationDate) {
 
 }
 
--- src/main/java/com/powerup/realestate/properties/application/dto/response/PropertyResponse.java
+++ src/main/java/com/powerup/realestate/properties/application/dto/response/PropertyResponse.java
@@ -3,8 +3,8 @@ package com.powerup.realestate.properties.application.dto.response;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record PropertyResponse(Long id, String name,String description,Long categoryId,Integer rooms, Integer bathrooms,
-    BigDecimal price, Long locationId, LocalDate activePublicationDate,
-    String publicationStatus, LocalDate publicationDate) {
+public record PropertyResponse(Long id, String name,String description,Long category,Integer rooms, Integer bathrooms,
+    BigDecimal price, Long location, LocalDate activePublicationDate,
+    String publicationStatus) {
 
     }
