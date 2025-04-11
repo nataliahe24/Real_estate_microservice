045d88be86c1f30bce1540fbbed74f0458fc431a
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-10 22:56:19 -0500

    refactor: add address

--- src/main/java/com/powerup/realestate/properties/application/dto/request/SavePropertyRequest.java
+++ src/main/java/com/powerup/realestate/properties/application/dto/request/SavePropertyRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record SavePropertyRequest(@NotBlank String name,@NotBlank String description,@NotBlank Long category,@NotBlank Integer rooms,@NotBlank Integer bathrooms,
+public record SavePropertyRequest(@NotBlank String name,@NotBlank String address, @NotBlank String description,@NotBlank Long category,@NotBlank Integer rooms,@NotBlank Integer bathrooms,
                                   @NotBlank BigDecimal price, @NotBlank Long location,@NotBlank LocalDate activePublicationDate) {
 
 }
