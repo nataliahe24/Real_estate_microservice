8bbb5d8805b32465c78163100f4a8b2369b376a8
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-10 22:35:00 -0500

    feat: add getPropertiesByFiltersAndOrder method to PropertyService

--- src/main/java/com/powerup/realestate/properties/application/services/PropertyService.java
+++ src/main/java/com/powerup/realestate/properties/application/services/PropertyService.java
@@ -3,10 +3,25 @@ package com.powerup.realestate.properties.application.services;
 
 
 import com.powerup.realestate.properties.application.dto.request.SavePropertyRequest;
+import com.powerup.realestate.properties.application.dto.response.PropertyResponse;
 import com.powerup.realestate.properties.application.dto.response.SavePropertyResponse;
+import com.powerup.realestate.properties.domain.utils.page.PageResult;
 
 
 public interface PropertyService {
     SavePropertyResponse save(SavePropertyRequest request);
+    PageResult<PropertyResponse> getPropertiesByFiltersAndOrder(
+            Integer page,
+            Integer size,
+            Long location,
+            Long category,
+            Integer rooms,
+            Integer bathrooms,
+            Double minPrice,
+            Double maxPrice,
+            String sortBy,
+            boolean orderAsc
+
+    );
 }
 
