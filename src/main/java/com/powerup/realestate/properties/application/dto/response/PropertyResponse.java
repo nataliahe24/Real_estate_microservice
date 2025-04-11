d7fd52773d138a64e4aabfabb8cbd747fe3e1b44
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-10 22:36:18 -0500

    feat: add PropertyResponse

--- src/main/java/com/powerup/realestate/properties/application/dto/response/PropertyResponse.java
+++ src/main/java/com/powerup/realestate/properties/application/dto/response/PropertyResponse.java
@@ -3,7 +3,7 @@ package com.powerup.realestate.properties.application.dto.response;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record PropertyResponse(Long id, String name,String description,Long category,Integer rooms, Integer bathrooms,
+public record PropertyResponse(Long id, String name,String address, String description,Long category,Integer rooms, Integer bathrooms,
     BigDecimal price, Long location, LocalDate activePublicationDate,
     String publicationStatus) {
 
