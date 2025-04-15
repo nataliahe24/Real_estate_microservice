10423423d5fb1d75e9a8c66cfab142006e25d458
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-15 14:53:46 -0500

    feat: implement LocationAlreadyExist

--- src/main/java/com/powerup/realestate/location/domain/utils/constants/LocationDomainConstants.java
+++ src/main/java/com/powerup/realestate/location/domain/utils/constants/LocationDomainConstants.java
@@ -7,7 +7,5 @@ public final class LocationDomainConstants {
 
     public static final String FIELD_CITY_NULL_MESSAGE = "El campo 'Ciudad' es obligatorio";
     public static final String FIELD_NEIGHBORHOOD_NULL_MESSAGE = "El campo 'Barrio' es obligatorio";
-
-
-
+    public static final String FIELD_LOCATION_ALREADY_EXIST_MESSAGE = "Ubicación ya existe";
 }
--- src/main/java/com/powerup/realestate/location/infrastructure/exceptionshandler/LocationControllerAdvisor.java
+++ src/main/java/com/powerup/realestate/location/infrastructure/exceptionshandler/LocationControllerAdvisor.java
@@ -1,8 +1,8 @@
 package com.powerup.realestate.location.infrastructure.exceptionshandler;
 
 
-
 import com.powerup.realestate.location.domain.exceptions.CityNonExistentException;
+import com.powerup.realestate.location.domain.exceptions.LocationAlreadyExist;
 import com.powerup.realestate.location.domain.exceptions.NeighborhoodNonNullException;
 import com.powerup.realestate.location.domain.utils.constants.LocationDomainConstants;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +15,22 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class LocationControllerAdvisor {
     @ExceptionHandler(CityNonExistentException.class)
-    public ResponseEntity<ExceptionResponse> handleCityNonExistentException(CityNonExistentException exception){
-        return  ResponseEntity.badRequest().body(new ExceptionResponse(ExceptionConstants.CITY_NON_EXISTS_EXCEPTION, LocalDateTime.now()));
+    public ResponseEntity<ExceptionResponse> handleCityNonExistentException(CityNonExistentException exception) {
+        return ResponseEntity.badRequest().body(new ExceptionResponse(
+                ExceptionConstants.CITY_NON_EXISTS_EXCEPTION, LocalDateTime.now()));
     }
+
     @ExceptionHandler(NeighborhoodNonNullException.class)
-    public ResponseEntity<ExceptionResponse> handleNeighborhoodNonNullException(NeighborhoodNonNullException exception){
-        return  ResponseEntity.badRequest().body(new ExceptionResponse(LocationDomainConstants.FIELD_NEIGHBORHOOD_NULL_MESSAGE, LocalDateTime.now()));
+    public ResponseEntity<ExceptionResponse> handleNeighborhoodNonNullException(NeighborhoodNonNullException exception) {
+        return ResponseEntity.badRequest().body(new ExceptionResponse(
+                LocationDomainConstants.FIELD_NEIGHBORHOOD_NULL_MESSAGE, LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(LocationAlreadyExist.class)
+    public ResponseEntity<ExceptionResponse> handleLocationAlreadyExist(LocationAlreadyExist exception) {
+        return ResponseEntity.badRequest().body(
+                new ExceptionResponse(LocationDomainConstants.FIELD_LOCATION_ALREADY_EXIST_MESSAGE, LocalDateTime.now())
+        );
     }
 }
 
