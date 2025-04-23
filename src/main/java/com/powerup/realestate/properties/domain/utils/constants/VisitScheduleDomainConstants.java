8c46a22079ab70ac26f1c4f06ab7a08e0d664c27
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-23 18:37:25 -0500

    feat: add VisitScheduleDomainConstants

---
+++ src/main/java/com/powerup/realestate/properties/domain/utils/constants/VisitScheduleDomainConstants.java
@@ -0,0 +1,12 @@
+package com.powerup.realestate.properties.domain.utils.constants;
+
+public final class VisitScheduleDomainConstants {
+    public static final String FIELD_SELLER_ID_NULL_MESSAGE = "El ID del vendedor no puede ser nulo.";
+    public static final String FIELD_PROPERTY_NULL_MESSAGE = "La propiedad no puede ser nula.";
+    public static final String FIELD_START_DATE_NULL_MESSAGE = "La fecha de inicio no puede ser nula.";
+    public static final String FIELD_END_DATE_NULL_MESSAGE = "La fecha de fin no puede ser nula.";
+    public static final String INVALID_FUTURE_DATE_MESSAGE = "La fecha de visita debe estar dentro de las próximas 3 semanas.";
+    public static final String INVALID_END_DATE_MESSAGE = "La fecha de fin debe ser posterior a la fecha de inicio.";
+    public static final String SAVE_VISIT_SCHEDULE_RESPONSE_MESSAGE = "Horario de visita creado exitosamente.";
+    public static final String UNAUTHORIZED_SELLER_MESSAGE = "El vendedor no es propietario de esta propiedad.";
+} 
