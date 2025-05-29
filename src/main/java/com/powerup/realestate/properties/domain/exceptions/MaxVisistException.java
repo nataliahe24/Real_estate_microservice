1e72fbecb827fedce553bfdb0dc9ce953d756155
Natalia Henao <nataliahenaor24@gmail.com>
2025-05-29 16:35:01 -0500

    refactor: rename MaxVisistException

--- src/main/java/com/powerup/realestate/properties/domain/exceptions/MaxVisistException.java
+++
@@ -1,7 +0,0 @@
-package com.powerup.realestate.properties.domain.exceptions;
-
-public class MaxVisistException extends RuntimeException {
-    public MaxVisistException(String message) {
-        super(message);
-    }
-}
