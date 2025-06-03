144d8d218727153d83d49e43eefed88db129e5bb
Natalia Henao <nataliahenaor24@gmail.com>
2025-06-03 15:04:23 -0500

    feat: rename

--- src/main/java/com/powerup/realestate/properties/application/dto/request/VisitScheduleRequest.java
+++ src/main/java/com/powerup/realestate/properties/application/dto/request/VisitScheduleRequest.java
@@ -2,7 +2,7 @@ package com.powerup.realestate.properties.application.dto.request;
 
 import java.time.LocalDateTime;
 
-public record FilterVisitScheduleRequest(
+public record VisitScheduleRequest(
     LocalDateTime startDate,
     LocalDateTime endDate,
     String location,
