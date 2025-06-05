cd249eedd528c0c5090c0ef03c9f04b52c52b967
Natalia Henao <nataliahenaor24@gmail.com>
2025-06-05 16:31:50 -0500

    refactor: delete spaces and imports

--- src/main/java/com/powerup/realestate/properties/application/dto/response/SavePropertyResponse.java
+++ src/main/java/com/powerup/realestate/properties/application/dto/response/SavePropertyResponse.java
@@ -2,5 +2,8 @@ package com.powerup.realestate.properties.application.dto.response;
 
 import java.time.LocalDateTime;
 
-public record SavePropertyResponse(String message, LocalDateTime time) {
+public record SavePropertyResponse(
+        String message,
+        LocalDateTime time
+) {
 }
