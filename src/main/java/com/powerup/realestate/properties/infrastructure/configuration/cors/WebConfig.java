3fe3b7889ac8b29eb6c7be5854c9ebc83f5b795c
Natalia Henao <nataliahenaor24@gmail.com>
2025-06-05 19:14:46 -0500

    refactor: move to folder

--- src/main/java/com/powerup/realestate/properties/infrastructure/configuration/cors/WebConfig.java
+++ src/main/java/com/powerup/realestate/properties/infrastructure/configuration/cors/WebConfig.java
@@ -1,4 +1,4 @@
-package com.powerup.realestate.properties.infrastructure.config;
+package com.powerup.realestate.properties.infrastructure.configuration.cors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
